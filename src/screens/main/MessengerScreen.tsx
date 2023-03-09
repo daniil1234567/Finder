47a35c7034f60a711746f4b59e256870f6fefedc
danii <daniil@poliven.ru>
2023-03-09 11:09:04 +0300

    screens

---
+++ src/screens/main/MessengerScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MessengerScreen = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MessengerScreen;
---
+++ src/screens/main/PageScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageScreen = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PageScreen;
